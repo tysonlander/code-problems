f8272c713aa60b906795ff1fe2de39f0ba869a36
tysonlander <tysonlander@gmail.com>
2019-11-08 11:29:57 -0700

    reverse array

--- functions.java
+++ functions.java
@@ -109,4 +109,18 @@ public class Main {
         }
         return false;
     }
+
+    // reverse an array
+    public static int[] reverseArray(int[] array){
+
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+        // 3, 4, 5, 6
+        for(int i = 0; i< halfLength; i++){
+            int temp = array[i];
+            array[i] = array[maxIndex- 1];
+            array[maxIndex - i ] = temp;
+        }
+        return array;
+    }
 }
